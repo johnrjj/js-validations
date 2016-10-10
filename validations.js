31f04580d7d2629994a8c7aee7dae7d1246ad29b
John Johnson <johnrjj@gmail.com>
2016-10-10 10:04:28 -0400

    Add files via upload

---
+++ validations.js
@@ -0,0 +1,64 @@
+const isExisty = (value) => value !== null && value !== undefined;
+
+const isEmpty = (value) => value === '';
+
+const isRequired = (value) => !isEmpty(value);
+
+const matchRegexp = (value, regexp) => !isExisty(value) || isEmpty(value) || regexp.test(value);
+
+const isUndefined = (value) => value === undefined;
+
+const isEmptyString = (value) => isEmpty(value);
+
+const isTrue = (value) => value === true;
+
+const isFalse = (value) => value === false;
+
+const isAlpha = (value) => matchRegexp(value, /^[A-Z]+$/i);
+
+const isAlphanumeric = (value) => matchRegexp(value, /^[0-9A-Z]+$/i);
+
+const isInt = (value) => matchRegexp(value, /^(?:[-+]?(?:0|[1-9]\d*))$/);
+
+const isFloat = (value) => matchRegexp(value, /^(?:[-+]?(?:\d+))?(?:\.\d*)?(?:[eE][\+\-]?(?:\d+))?$/);
+
+const isWords = (value) => matchRegexp(value, /^[A-Z\s]+$/i);
+
+const isLength = (value) => !isExisty(value) || isEmpty(value) || value.length === length;
+
+const equals = (value, otherValue) => !isExisty(value) || isEmpty(value) || value == otherValue;
+
+const maxLength = (value, length) => !isExisty(value) || value.length <= length;
+
+const minLength = (value, length) => !isExisty(value) || isEmpty(value) || value.length >= length;
+
+const isNumeric = (value) => typeof value === 'number' ? true : matchRegexp(value, /^[-+]?(?:\d*[.])?\d+$/);
+
+const isEmail = (value) => {
+  return matchRegexp(value, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i);
+};
+
+const isUrl = (value) => {
+  return matchRegexp(value, /^(https?|s?ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i);
+};
+
+export {
+  isEmpty,
+  isRequired,
+  isUndefined,
+  isEmptyString,
+  isTrue,
+  isFalse,
+  isNumeric,
+  isAlpha,
+  isAlphanumeric,
+  isInt,
+  isFloat,
+  isWords,
+  isLength,
+  equals,
+  maxLength,
+  minLength,
+  isEmail,
+  isUrl,
+};
